162967c0c99939539e86247af984585a0466f937
Deniss <den.b@itservicesgroup.co.uk>
2017-09-08 13:02:58 +0100

    Add Gulp task for browserify and reactify

--- gulpfile.js
+++ gulpfile.js
@@ -4,39 +4,56 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var open = require('gulp-open');
 
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+
 var config = {
-  port: 3000,
-  devBaseUrl: 'http://localhost',
-  paths: {
-    html: './src/*.html',
-    dist: './dist'
-  }
+	port: 3000,
+	devBaseUrl: 'http://localhost',
+	paths: {
+		html: './src/*.html',
+		js: './src/**/*.js',
+		dist: './dist',
+		mainJs: './src/main.js'
+	}
 };
 
 gulp.task('connect', function () {
-  connect.server({
-    root: ['dist'],
-    port: config.port,
-    base: config.devBaseUrl,
-    livereload: true
-  });
+	connect.server({
+		root: ['dist'],
+		port: config.port,
+		base: config.devBaseUrl,
+		livereload: true
+	});
 });
 
 gulp.task('open', ['connect'], function () {
-  gulp.src('dist/index.html')
-    .pipe(open(' ', {
-      url: config.devBaseUrl + ':' + config.port + '/'
-    }));
+	gulp.src('dist/index.html')
+		.pipe(open(' ', {
+			url: config.devBaseUrl + ':' + config.port + '/'
+		}));
 });
 
 gulp.task('html', function () {
-  gulp.src(config.paths.html)
-    .pipe(gulp.dest(config.paths.dist))
-    .pipe(connect.reload());
+	gulp.src(config.paths.html)
+		.pipe(gulp.dest(config.paths.dist))
+		.pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+	browserify(config.paths.mainJs)
+		.transform(reactify)
+		.bundle()
+		.on('error', console.error.bind(console))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
+		.pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js']);
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);
